784bcd5ccf6987b203bb72b665b3c51d625a8afd
rabiakurandatum <59939284+RabiaKuran@users.noreply.github.com>
2022-11-04 17:59:42 +0300

    KisiselBolumtamamlandı

--- src/pages/cards/hospital/PersonalProductsCard.tsx
+++ src/pages/cards/hospital/PersonalProductsCard.tsx
@@ -5,12 +5,9 @@ import ImageListItemBar from "@mui/material/ImageListItemBar";
 import ListSubheader from "@mui/material/ListSubheader";
 import IconButton from "@mui/material/IconButton";
 import InfoIcon from "@mui/icons-material/Info";
-import { Stack, LinearProgress, Alert, AlertTitle } from "@mui/material";
-import AButton from "../../../components/buttons/AButton";
 import AGridItem from "../../../components/grids/AGridItem";
 import "../../../components/labels/horizontalRule/horizontalRule.css";
 import RedirectHelper from "../../../helper/RedirectHelper";
-import CommentIcon from "@mui/icons-material/Comment";
 import ACard from "../../../components/cards/ACard";
 import AGrid from "../../../components/grids/AGrid";
 import AHeaderLabel from "../../../components/labels/header/AHeaderLabel";
@@ -21,12 +18,20 @@ import ATableHead from "../../../components/tables/ATableHead";
 import ATableRow from "../../../components/tables/ATableRow";
 import HoverStyledTableCell from "../../../components/tables/HoverStyledTableCell";
 import { ColorPalette } from "../../../theme/ColorPalette";
-import { minHeight } from "@mui/system";
-import AverageRecencyCltvPRecencyCard from "../averageRecencyCltvPRecency/AverageRecencyCltvPRecencyCard";
+import { useState } from "react";
+import ProductsModel from "../../../models/products/ProductsModel";
+import ProductsService from "../../../services/products/ProductsService";
 
 interface IPersonalProductsCard {
   progress: Boolean;
   button: Boolean;
+}
+
+export interface AverageRecencyCltvPRecencyItem {
+  urunId: number;
+  urunKategori: string;
+  urunAdi: string;
+  urunAdedi: number;
 }
 
 export default function PersonalProductsCard(props: IPersonalProductsCard) {
@@ -36,169 +41,130 @@ export default function PersonalProductsCard(props: IPersonalProductsCard) {
     RedirectHelper.redirect("/dashboard");
   };
 
+  const [averageRecencyCltvPRecency, setAverageRecencyCltvPRecency] =
+    useState<ProductsModel[]>();
+  const [loading, setLoading] = React.useState(true);
+  const [dataSource, setDataSource] = React.useState<any>([]);
+
+  React.useEffect(() => {
+    getAverageRecencyCltvPRecency();
+  }, []);
+
+  const getAverageRecencyCltvPRecency = async () => {
+    try {
+      var averageRecencyCltvPRecency = await ProductsService.getProducts();
+      const data: AverageRecencyCltvPRecencyItem[] = [];
+      averageRecencyCltvPRecency.forEach((item) => {
+        data.push({
+          urunId: item.urunId,
+          urunKategori: item.urunKategori,
+          urunAdi: item.urunAdi,
+          urunAdedi: item.urunAdedi,
+        });
+      });
+      setDataSource(data);
+      setAverageRecencyCltvPRecency(averageRecencyCltvPRecency);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <ACard sx={{ marginTop: "12px" }}>
       <AGrid>
-      
-      <AGridItem xs={9} sm={12} md={6} xl={6} minWidth={1400}>
-        <ImageList sx={{ margin: "12px" }}>
-          <ImageListItem key="Subheader" cols={2}>
-            <ListSubheader component="div">
-              Kişisel Ürünler Listesi
-            </ListSubheader>
-          </ImageListItem>
-          {itemData.map((item) => (
-            <ImageListItem key={item.img}>
-              <img
-                src={`${item.img}?w=248&fit=crop&auto=format`}
-                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                alt={item.title}
-                loading="lazy"
-              />
-              <ImageListItemBar
-                title={item.title}
-                subtitle={item.author}
-                actionIcon={
-                  <IconButton
-                    sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                    aria-label={`info about ${item.title}`}
-                  >
-                    <InfoIcon />
-                  </IconButton>
-                }
-              />
+        <AGridItem xs={9} sm={12} md={6} xl={6} minWidth={1400}>
+          <ImageList sx={{ margin: "12px" }}>
+            <ImageListItem key="Subheader" cols={2}>
+              <ListSubheader component="div">
+                Kişisel Ürünler Listesi
+              </ListSubheader>
             </ImageListItem>
-          ))}
-        </ImageList>
-      </AGridItem>
-      <AGridItem sx={{ overflow: "hidden"  }} minWidth={'%60'} marginTop={3} xs={4}>
-            <ATableContainer >
-              <ATable className="basic">
-                <ATableHead>
-                  <ATableRow>
-                    <HoverStyledTableCell>
-                      <AHeaderLabel size={5} color={ColorPalette.gray}>
-                        Ürün Id
-                      </AHeaderLabel>
-                    </HoverStyledTableCell>
-                    <HoverStyledTableCell>
-                      <AHeaderLabel size={5} color={ColorPalette.gray}>
-                        Ürün Kategori
-                      </AHeaderLabel>
-                    </HoverStyledTableCell>
+            {itemData.map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                  src={`${item.img}?w=248&fit=crop&auto=format`}
+                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.title}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={item.title}
+                  subtitle={item.author}
+                  actionIcon={
+                    <IconButton
+                      sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                      aria-label={`info about ${item.title}`}
+                    >
+                      <InfoIcon />
+                    </IconButton>
+                  }
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </AGridItem>
+        <AGridItem
+          sx={{ overflow: "hidden" }}
+          minWidth={"%60"}
+          marginTop={3}
+          xs={4}
+        >
+          <ATableContainer>
+            <ATable className="basic">
+              <ATableHead>
+                <ATableRow>
+                  <HoverStyledTableCell>
+                    <AHeaderLabel size={5} color={ColorPalette.black}>
+                      Ürün Id
+                    </AHeaderLabel>
+                  </HoverStyledTableCell>
+                  <HoverStyledTableCell>
+                    <AHeaderLabel size={5} color={ColorPalette.black}>
+                      Ürün Kategori
+                    </AHeaderLabel>
+                  </HoverStyledTableCell>
+                  <HoverStyledTableCell>
+                    <AHeaderLabel size={5} color={ColorPalette.black}>
+                      Ürün Adı
+                    </AHeaderLabel>
+                  </HoverStyledTableCell>
+                  <HoverStyledTableCell>
+                    <AHeaderLabel size={5} color={ColorPalette.black}>
+                      Ürün Adedi
+                    </AHeaderLabel>
+                  </HoverStyledTableCell>
+                </ATableRow>
+              </ATableHead>
 
-                    <HoverStyledTableCell>
-                      <AHeaderLabel size={5} color={ColorPalette.gray}>
-                        Ürün Adı
-                      </AHeaderLabel>
-                    </HoverStyledTableCell>
-                    <HoverStyledTableCell>
-                      <AHeaderLabel size={5} color={ColorPalette.gray}>
-                        Ürün Adedi
-                      </AHeaderLabel>
-                    </HoverStyledTableCell>
-                  </ATableRow>
-                </ATableHead>
+              {averageRecencyCltvPRecency?.map((row) => (
                 <ATableBody>
-                  <HoverStyledTableCell>{"123"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Havlu"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"240"}
-                  </HoverStyledTableCell>
+                  {row.urunKategori === "Kişisel Bölüm" ? (
+                    <ATableRow
+                      sx={{
+                        ":hover": {
+                          backgroundColor: "#F9F9FD",
+                        },
+                      }}
+                    >
+                      <HoverStyledTableCell>{row.urunId}</HoverStyledTableCell>
+                      <HoverStyledTableCell>
+                        {row.urunKategori}
+                      </HoverStyledTableCell>
+                      <HoverStyledTableCell>{row.urunAdi}</HoverStyledTableCell>
+                      <HoverStyledTableCell>
+                        {row.urunAdedi}
+                      </HoverStyledTableCell>
+                    </ATableRow>
+                  ) : (
+                   <AGrid></AGrid>
+                  )}
                 </ATableBody>
-                <ATableBody>
-                  <HoverStyledTableCell>{"146"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Nevresim"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"500"}
-                  </HoverStyledTableCell>
-                </ATableBody>
-                <ATableBody>
-                  <HoverStyledTableCell>{"123"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Çarşaf"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"220"}
-                  </HoverStyledTableCell>
-                </ATableBody>
-                <ATableBody>
-                  <HoverStyledTableCell>{"123"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Yastık"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"140"}
-                  </HoverStyledTableCell>
-                </ATableBody>
-                <ATableBody>
-                  <HoverStyledTableCell>{"123"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Terlik"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"23"}
-                  </HoverStyledTableCell>
-                </ATableBody>
-                <ATableBody>
-                  <HoverStyledTableCell>{"123"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Saç Yıkama Bonesi"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"203"}
-                  </HoverStyledTableCell>
-                </ATableBody>
-                <ATableBody>
-                  <HoverStyledTableCell>{"123"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Yorgan"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"523"}
-                  </HoverStyledTableCell>
-                </ATableBody>
-                <ATableBody>
-                  <HoverStyledTableCell>{"123"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Yastık Kılıfı"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"139"}
-                  </HoverStyledTableCell>
-                </ATableBody>
-                <ATableBody>
-                  <HoverStyledTableCell>{"123"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Alez"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"213"}
-                  </HoverStyledTableCell>
-                </ATableBody>
-                <ATableBody>
-                  <HoverStyledTableCell>{"123"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Diş macunu ve diş fırçası"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"623"}
-                  </HoverStyledTableCell>
-                </ATableBody>
-                <ATableBody>
-                  <HoverStyledTableCell>{"123"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Şampuan"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"130"}
-                  </HoverStyledTableCell>
-                </ATableBody>
-                <ATableBody>
-                  <HoverStyledTableCell>{"123"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Kişisel Bölüm"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>{"Peçete"}</HoverStyledTableCell>
-                  <HoverStyledTableCell>
-                    {"2378"}
-                  </HoverStyledTableCell>
-                </ATableBody>
-              </ATable>
-            </ATableContainer>
-            <AverageRecencyCltvPRecencyCard></AverageRecencyCltvPRecencyCard>
-          </AGridItem>
+              ))}
+            </ATable>
+          </ATableContainer>
+        </AGridItem>
       </AGrid>
     </ACard>
   );
@@ -218,7 +184,7 @@ const itemData = [
     title: "Nevresim",
     author: "@erisim",
   },
-  
+
   {
     img: "https://www.vitateks.com/images/carsaf4.jpg",
     title: "Çarşaf",
@@ -269,7 +235,7 @@ const itemData = [
   {
     img: "https://ae01.alicdn.com/kf/H7ce43bf6c43b41b3a10bfaed95921d73m/S-cak-10-ml-ampuan-Seyahat-Boy-Plastik-i-eler-10-ml-PET-Plastik-ampuan-i.jpg_Q90.jpg_.webp",
     title: "Şampuan",
-    author:"@erisim",
+    author: "@erisim",
   },
   {
     img: "https://www.sarfmarket.com.tr/tork-matic-hareketli-havlu-advanced-150-m-6li-fiyati-fotoselli-makine-havlulari-tork-290067-18170-38-K.jpg",
